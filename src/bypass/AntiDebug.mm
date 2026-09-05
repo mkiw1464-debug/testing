d6ee365dca4ccd761d7b3c855e9b23ef8ee9049e
mkiw1464-debug <mkiw1464@gmail.com>
2026-09-06 04:28:08 +0800

    Add anti-debugging functionality in AntiDebug.mm
    Implement anti-debugging techniques by hooking ptrace and sysctl.

---
+++ src/bypass/AntiDebug.mm
@@ -0,0 +1,32 @@
+#import "AntiDebug.h"
+#import <sys/ptrace.h>
+#import <sys/sysctl.h>
+
+// Block ptrace anti-debug
+static int (*orig_ptrace)(int, pid_t, caddr_t, int) = nullptr;
+static int fake_ptrace(int req, pid_t pid, caddr_t addr, int data) {
+    if (req == PT_DENY_ATTACH) return 0; // deny the deny
+    return orig_ptrace(req, pid, addr, data);
+}
+
+// Block sysctl debugger check
+static int (*orig_sysctl)(int*, u_int, void*, size_t*, void*, size_t) = nullptr;
+static int fake_sysctl(int *name, u_int nlen, void *oldp,
+                       size_t *oldlenp, void *newp, size_t newlen) {
+    int ret = orig_sysctl(name, nlen, oldp, oldlenp, newp, newlen);
+    if (nlen == 4 && name[0] == CTL_KERN && name[1] == KERN_PROC
+        && name[2] == KERN_PROC_PID && oldp) {
+        struct kinfo_proc *info = (struct kinfo_proc*)oldp;
+        info->kp_proc.p_flag &= ~P_TRACED;
+    }
+    return ret;
+}
+
+void InitAntiDebug() {
+    // fishhook
+    struct rebinding r[] = {
+        {"ptrace",  (void*)fake_ptrace,  (void**)&orig_ptrace},
+        {"sysctl",  (void*)fake_sysctl,  (void**)&orig_sysctl},
+    };
+    rebind_symbols(r, 2);
+}
