f068ad6fd389fbe95aab545a59a1c65ee8bb299f
mkiw1464-debug <mkiw1464@gmail.com>
2026-09-06 04:27:01 +0800

    Add Bypass.h with function declarations

---
+++ src/bypass/Bypass.h
@@ -0,0 +1,14 @@
+#pragma once
+
+void InitAllBypasses();
+void BypassAntiBan();
+void BypassAntiCheat();
+void BypassLogin();
+void BypassLobby();
+void BypassReport();
+void BypassBlacklist();
+void BypassThirdPartyInstall(); // esign/gbox/ksign/feather
+void PatchIntegrityCheck();
+void SpoofDeviceFingerprint();
+void HideLibraryFromTaskList();
+void PatchJailbreakDetection();
